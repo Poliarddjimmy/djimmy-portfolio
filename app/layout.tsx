7b4f9384ceefa49dfefb8e86004a5aaacc6bb6a8
Djimmy Poliard <djimmypoliard@gmail.com>
2025-03-17 21:04:13 -0400

    sd

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   title: "Djimmy Poliard | Full-Stack Software Developer",
   description:
     "Full-stack Developer with 8+ years of experience building scalable web applications and internal tools that drastically improve efficiency.",
+    icons: {
+      icon: "/favicon.ico", // This points to the favicon in your public folder
+    },
 }
 
 export default function RootLayout({
